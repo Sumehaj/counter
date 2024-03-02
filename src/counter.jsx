247e84bdb6af3ee0136a86197a39290f8102cdfe
Sumehaj <jakatesumeha1309@gmail.com>
2024-03-02 16:23:49 +0530

    asd

---
+++ src/counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Counter(){
+    const [count,setCount]=useState(0)
+    const handleIncrement=() =>{
+        setCount(count+1)
+        console.log(count)
+    }
+    const handleDecrement=() =>{
+        setCount(count-1)
+        console.log(count)
+    }
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+        </div>
+    )
+}
+export default Counter;
